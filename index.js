e77de3efbc6fc1e794b184b68be3e12b16f22f42
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-10-15 20:42:33 -0700

    moved generateShape to shapes.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateShape = require('./lib/shapes.js')
 
 const questions = [
     {
@@ -31,17 +32,6 @@ const questions = [
         message:'Enter a shape color(OR a hexadecimal number):',
     }
 ];
-
-function generateShape(shape, shapeColor) {
-    switch (shape) {
-        case 'circle':
-            return `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
-        case 'triangle':
-            return `<polygon points="150,40 100,130 200,130" fill="${shapeColor}" />`;
-        case 'square':
-            return `<rect x="100" y="50" width="100" height="100" fill="${shapeColor}" />`;
-    }
-}
 
 function init() {
     inquirer.prompt(questions).then((answers)=> {
